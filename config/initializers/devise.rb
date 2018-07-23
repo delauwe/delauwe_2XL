8d18e32d8315fcd17cd93ef7a900465b57592925
Elsa Szymczak <elsaszymczak@gmail.com>
2018-07-23 15:33:55 +0200

    facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,11 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+    scope: 'email',
+    info_fields: 'email, first_name, last_name',
+    image_size: 'square',  # 50x50, guaranteed ratio
+    secure_image_url: true
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -257,7 +262,6 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "APP_ID", "APP_SECRET", scope: 'user,public_repo'
 
 
   # ==> Warden configuration
--- db/migrate/20180719133722_remove_photo_from_apartment.rb
+++ db/migrate/20180719133722_remove_photo_from_apartment.rb
@@ -1,6 +1,5 @@
 class RemovePhotoFromApartment < ActiveRecord::Migration[5.2]
   def change
     remove_column :apartments, :photo
-
   end
 end
