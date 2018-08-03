4e2ce31f1b1315cf74c6f5ab3dd9b5287e6a9284
Elsa Szymczak <elsaszymczak@gmail.com>
2018-08-03 11:59:45 +0200

    added gem jqueri mobile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.mobile
+
 import "bootstrap";
 import { cardCarousel } from "../components/card-carousel";
 import { swipeCarousel } from "../components/swipe-carousel";
