a889de525dccf12d70d9595d8cdbf0c719f64ab0
Elsa Szymczak <elsaszymczak@gmail.com>
2018-08-29 17:48:53 +0200

    migration

--- db/migrate/20180829091733_change_column_name_from_apartment_translations.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNameFromApartmentTranslations < ActiveRecord::Migration[5.2]
-  def change
-        rename_column :apartment_translations, :category, :nature
-
-  end
-end
--- db/migrate/20180829153536_drop_translation_table.rb
+++
@@ -1,5 +0,0 @@
-class DropTranslationTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :apartment_translations
-  end
-end
