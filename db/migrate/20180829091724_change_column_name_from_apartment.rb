f457e9fae15a8eae5b03dd37288ae431b8d5d777
Elsa Szymczak <elsaszymczak@gmail.com>
2018-08-29 17:26:57 +0200

    changed column name

--- db/migrate/20180829091724_change_column_name_from_apartment.rb
+++ db/migrate/20180829091724_change_column_name_from_apartment.rb
@@ -1,5 +1,5 @@
 class ChangeColumnNameFromApartment < ActiveRecord::Migration[5.2]
   def change
-    rename_column :apartments, :type, :nature
+    rename_column :apartments, :category, :nature
   end
 end
