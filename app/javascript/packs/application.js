ed4ecac1b175a19031f730d89114b39c1d992d69
Elsa Szymczak <elsaszymczak@gmail.com>
2018-09-13 14:28:04 +0200

    like/dislike link to form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ import "bootstrap";
 import { cookies } from "../components/cookies";
 import { sweetAlertReservation } from '../components/sweet-alert';
 import { sweetAlertContact } from '../components/sweet-alert';
+import { counter } from "../components/counter";
+
 
 
 cookies();
 sweetAlertReservation();
 sweetAlertContact();
+counter();
