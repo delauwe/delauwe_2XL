e185a40b5d20fd3240ec57745f49edb743ee8683
Elsa <elsaszymczak@gmail.com>
2023-07-04 17:31:30 +0200

    Edit contact page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,7 @@
 import "bootstrap";
 
 import { cookies } from "../components/cookies";
-import { sweetAlertReservation } from '../components/sweet-alert';
-import { sweetAlertContact } from '../components/sweet-alert';
 import { counter } from "../components/counter";
 
-
-
 cookies();
-sweetAlertReservation();
-sweetAlertContact();
 counter();
